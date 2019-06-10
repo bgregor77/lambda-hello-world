fdaac48e104697798cbae087f1cfc3acf484eafa
Bryan Gregorius <bgregorius@pivotal.io>
2019-06-10 12:28:29 -0400

    change

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello Cambridge!",
+            "message": "Hello Bryan!",
             "location": ip.text.replace("\n", "")
         }),
     }
